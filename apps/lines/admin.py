743d0e20e613a1079cd63f03d782904eacd483c4
Ezequiel Flores <supereze.bxb@gamil.com>
2018-12-13 09:19:03 -0600

    Agregando modelos a admin para visualizar, configuracion antes de migraciones

--- apps/lines/admin.py
+++ apps/lines/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import LineModel, RouteModel
 
-# Register your models here.
+admin.site.register(LineModel)
+admin.site.register(RouteModel)
--- apps/stations/admin.py
+++ apps/stations/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import LocationModel, StationModel
 
-# Register your models here.
+admin.site.register(LocationModel)
+admin.site.register(StationModel)
